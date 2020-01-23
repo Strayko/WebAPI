222958eeee8be8cb6b5c1e661662be6d37d0256d
Strayko <strayko_lol@windowslive.com>
2020-01-23 15:31:07 +0100

    models instead of entities

--- TheCodeCamp/Controllers/CampController.cs
+++ TheCodeCamp/Controllers/CampController.cs
@@ -1,32 +1,40 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using AutoMapper;
 using TheCodeCamp.Data;
+using TheCodeCamp.Models;
 
 namespace TheCodeCamp.Controllers
 {
     public class CampController : ApiController
     {
         private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
 
-        public CampController(ICampRepository repository)
+        public CampController(ICampRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
         public async Task<IHttpActionResult> Get()
         {
             try
             {
                 var result = await _repository.GetAllCampsAsync();
-                return Ok(result);
+
+                // Mapping
+                var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
+                return Ok(mappedResult);
             }
             catch (Exception ex)
             {
                 // TODO Add Logging
-                return InternalServerError(ex);   
+                return InternalServerError(ex);
             }
         }
     }
